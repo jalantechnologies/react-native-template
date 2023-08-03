2bd8b92e3250408eeef1238e30830a286adf8ce7
Naveen Kumar <naveen.umar@jalantechnologies.com>
2023-08-03 10:07:14 +0530

    fixed tests and renamed folder names to solve github issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Brand } from './brand/brand';
+export { default as Brand } from './my-brand/my-brand';
 export { default as CustomButton } from './custom-button/custom-button';
 export { default as ErrorFallback } from './error-fallback/error-fallback';

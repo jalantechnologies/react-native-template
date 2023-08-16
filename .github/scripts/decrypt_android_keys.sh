84527bbe3986402c10512f56f61b944e270bb41b
Naveen Kumar <naveen.kumar@jalantechnologies.com>
2023-08-16 15:08:02 +0530

    Testing android deployment

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -1,7 +1,7 @@
 echo "$RELEASE_KEYSTORE" > upload-keystore.jks.asc 
-gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch upload-keystore.jks.asc > opt/app/android/fastlane/config/upload-keystore.jks
+gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch upload-keystore.jks.asc > android/fastlane/config/upload-keystore.jks
 
 echo "$SERVICE_ACCOUNT" > service-account.json.asc 
-gpg -d --passphrase="$SERVICE_ACCOUNT_PASSPHRASE" --batch service-account.json.asc > opt/app/android/fastlane/config/service-account.json 
+gpg -d --passphrase="$SERVICE_ACCOUNT_PASSPHRASE" --batch service-account.json.asc > android/fastlane/config/service-account.json 
 
 rm upload-keystore.jks.asc service-account.json.asc

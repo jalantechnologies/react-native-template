a5555be848d9728a71f9d97e9210bdb31dd682ae
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-06-18 11:36:50 +0530

    feat: Add Dashboard screen component

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,6 @@
 export { default as Startup } from './startup/startup';
 export { default as Home } from './home/home';
+export { default as SignUp } from './auth/sign-up';
+export { default as OTP } from './auth/otp';
+export { default as Dashboard } from './dashboard/dashboard';
+export { default as Registration } from './auth/registration';

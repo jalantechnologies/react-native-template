25388b249576c9420f36fc6537d6f3c17d36b78f
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-07-25 18:17:14 +0530

    feat: Update app theme with custom button styles

--- src/app-theme.ts
+++ src/app-theme.ts
@@ -1,4 +1,4 @@
-import { extendTheme } from 'native-base';
+import { extendTheme, Modal } from 'native-base';
 
 const appTheme = extendTheme({
   colors: {
@@ -9,8 +9,33 @@ const appTheme = extendTheme({
   },
   components: {
     Button: {
-      defaultProps: {
-        bg: 'primary',
+      baseStyle: {
+        _text: {
+          color: 'white',
+        },
+      },
+      variants: {
+        solid: {
+          bg: 'primary',
+        },
+        subtle: {
+          bg: 'background',
+          _text: {
+            color: 'coolGray.800',
+          },
+          _pressed: {
+            bg: 'coolGray.200',
+          },
+        },
+        danger: {
+          bg: 'danger.600',
+          _disabled: {
+            bg: 'danger.200',
+          },
+          _pressed: {
+            bg: 'danger.700',
+          },
+        },
       },
     },
     Input: {
@@ -44,6 +69,13 @@ const appTheme = extendTheme({
         },
         '2xl': {
           color: 'white',
+        },
+      },
+    },
+    Toast: {
+      baseStyle: {
+        _title: {
+          textAlign: 'center',
         },
       },
     },

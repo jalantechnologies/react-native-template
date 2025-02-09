28ac214f3d23f7be135c06f34b2c1ffd5e1e3cb8
yuvraj singh jadon <yuvrajjadon10@gmail.com>
2025-02-09 21:03:56 +0530

    Add Typography component to index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as ErrorFallback } from './error-fallback/error-fallback';
 export { default as OTPInput } from './otp-input/otp-input';
 export { default as Spinner } from './spinner/spinner';
 export { default as FullScreenSpinner } from './spinner/full-screen-spinner';
+export { default as Typography } from './typography/typography';

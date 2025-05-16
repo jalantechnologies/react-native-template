70f91e883c312b84f2637db0055e65a1aa7b2359
Govind Sharma <govindgovind852@gmail.com>
2025-05-16 20:03:19 +0530

    feat: Update Modal header and footer

--- src/components/modal/modal-footer.component.tsx
+++ src/components/modal/modal-footer.component.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 
 import { styles } from './modal.styles';
+import { Nullable } from 'boilerplate-react-native/src/types';
 
 interface ModalFooterProps {
-  onCancel?: () => void;
-  onConfirm?: () => void;
+  onCancel?: Nullable<() => void>;
+  onConfirm?: Nullable<() => void>;
   cancelText?: string;
   confirmText?: string;
   isConfirmDisabled?: boolean;
+  children?: React.ReactNode;
 }
 
 const ModalFooter: React.FC<ModalFooterProps> = ({
@@ -17,29 +19,36 @@ const ModalFooter: React.FC<ModalFooterProps> = ({
   cancelText = 'Cancel',
   confirmText = 'Confirm',
   isConfirmDisabled = false,
+  children,
 }) => (
   <View style={styles.footerContainer}>
-    <TouchableOpacity style={styles.button} onPress={onCancel}>
-      <Text style={styles.cancelText}>{cancelText}</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={[styles.button, isConfirmDisabled && styles.disabledButton]}
-      onPress={onConfirm}
-      disabled={isConfirmDisabled}
-    >
-      <Text style={[styles.confirmText, isConfirmDisabled && styles.disabledText]}>
-        {confirmText}
-      </Text>
-    </TouchableOpacity>
+    {onCancel && (
+      <TouchableOpacity style={styles.button} onPress={onCancel}>
+        <Text style={styles.cancelText}>{cancelText}</Text>
+      </TouchableOpacity>
+    )}
+    {onConfirm && (
+      <TouchableOpacity
+        style={[styles.button, isConfirmDisabled && styles.disabledButton]}
+        onPress={onConfirm}
+        disabled={isConfirmDisabled}
+      >
+        <Text style={[styles.confirmText, isConfirmDisabled && styles.disabledText]}>
+          {confirmText}
+        </Text>
+      </TouchableOpacity>
+    )}
+    {children}
   </View>
 );
 
 ModalFooter.defaultProps = {
-  onCancel: () => {},
-  onConfirm: () => {},
+  onCancel: null,
+  onConfirm: null,
   cancelText: 'Cancel',
   confirmText: 'Confirm',
   isConfirmDisabled: false,
+  children: null,
 };
 
 export default ModalFooter;

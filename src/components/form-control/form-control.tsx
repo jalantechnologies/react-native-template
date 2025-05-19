cf9a11b46ee64980ef61459719aa18eba7e4a5a1
Govind Sharma <govindgovind852@gmail.com>
2025-05-19 18:30:48 +0530

    nit: fix typo in form-control styles file name

--- src/components/form-control/form-control.tsx
+++ src/components/form-control/form-control.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import { useFormControlStyles } from './form-vontrol.styles';
+import { useFormControlStyles } from './form-control.styles';
 
 interface FormControlProps {
   error?: string;

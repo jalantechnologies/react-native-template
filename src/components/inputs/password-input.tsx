3166b37d7ea847f5e0a6492b62a6733bdbf7d133
Govind Sharma <govindgovind852@gmail.com>
2025-05-25 12:50:26 +0530

    fix: Remove unnecessary `isPassword` prop from PasswordInput component

--- src/components/inputs/password-input.tsx
+++ src/components/inputs/password-input.tsx
@@ -6,7 +6,7 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Input, { InputProps } from './input';
 import { usePasswordInputStyles } from './input.styles';
 
-interface PasswordInputProps extends Omit<InputProps, 'isPassword'> {}
+interface PasswordInputProps extends InputProps {}
 
 const PasswordInput: React.FC<PasswordInputProps> = ({
   placeholder,

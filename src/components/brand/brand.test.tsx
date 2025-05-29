5e58d4256e52932a1ef98ea87a073715109b3764
Govind Sharma <govindgovind852@gmail.com>
2025-05-29 19:48:30 +0530

    fix: Add mock for svg import to fix test case

--- src/components/brand/brand.test.tsx
+++ src/components/brand/brand.test.tsx
@@ -9,6 +9,11 @@ const inset = {
   insets: { top: 0, left: 0, right: 0, bottom: 0 },
 };
 
+jest.mock('boilerplate-react-native/assets/img/logo.svg', () => {
+  const TestReact = require('react');
+  return () => TestReact.createElement('SvgMock');
+});
+
 test('App renders correctly', () => {
   const component = (
     <NativeBaseProvider initialWindowMetrics={inset}>

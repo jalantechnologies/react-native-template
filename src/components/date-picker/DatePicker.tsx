810974b4acb783b67d8a46922db3fdcc1c8c8fc2
Akshatbari02 <akshatbari.ab@gmail.com>
2025-06-11 18:31:47 +0530

    fix: fixed the format of code

--- src/components/date-picker/DatePicker.tsx
+++ src/components/date-picker/DatePicker.tsx
@@ -1,25 +1,17 @@
-import React, { useState } from 'react';
-import {
-  Platform,
-  View,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState } from 'react';
+import { Platform, View, Text, TouchableOpacity } from 'react-native';
 
-import { DatePickerProps } from './date-picker.types';
 import { styles } from './date-picker.styles';
+import { DatePickerProps } from './date-picker.types';
 
-export const DatePicker: React.FC<DatePickerProps> = ({
-  date,
-  onDateChange,
-  mode = 'date',
-}) => {
+export const DatePicker: React.FC<DatePickerProps> = ({ date, onDateChange, mode = 'date' }) => {
   const [show, setShow] = useState(false);
-
   const onChange = (_: any, selectedDate?: Date) => {
     setShow(false);
-    if (selectedDate) onDateChange(selectedDate);
+    if (selectedDate) {
+      onDateChange(selectedDate);
+    }
   };
 
   return (
@@ -28,13 +20,11 @@ export const DatePicker: React.FC<DatePickerProps> = ({
         <Text style={styles.text}>
           {mode === 'time'
             ? date.toLocaleTimeString()
-            : date.toLocaleDateString(
-                'en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-      }
-            )}
+            : date.toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              })}
         </Text>
       </TouchableOpacity>
 

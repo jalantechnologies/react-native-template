294eb8793e1328f1c07a79a71e30e6798e54ea3f
Akshatbari02 <akshatbari.ab@gmail.com>
2025-06-12 16:51:14 +0530

    fix: code format

--- src/logger/internals/types.ts
+++ src/logger/internals/types.ts
@@ -2,11 +2,11 @@ export default interface Logger {
   info(message: string): void;
 
   debug(message: string): void;
-  
+
   error(message: string): void;
-  
+
   warn(message: string): void;
-  
+
   critical(message: string): void;
 }
 

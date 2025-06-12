6c83f71714240f921b6e350f46d450d86b92f355
Akshatbari02 <akshatbari.ab@gmail.com>
2025-06-12 16:11:27 +0530

    fix: code format

--- src/components/date-picker/date-picker.styles.ts
+++ src/components/date-picker/date-picker.styles.ts
@@ -1,23 +1,23 @@
 import { useTheme } from 'native-base';
 import { StyleSheet } from 'react-native';
 
-export const DatePickerStyles = ()=>{
+export const DatePickerStyles = () => {
   const theme = useTheme();
 
   return StyleSheet.create({
     container: {
-        marginVertical: theme.space['8'],
-        marginHorizontal: theme.space['8'],
-      },
-      button: {
-        paddingVertical: theme.space['3'],
-        paddingHorizontal: theme.space['4'],
-        backgroundColor: theme.colors.gray[100],
-        borderRadius: theme.radii?.sm ?? 6,
-        alignItems: 'center',
-      },
-      text: {
-        fontSize: theme.fontSizes?.md ?? 16,
-      },
+      marginVertical: theme.space['8'],
+      marginHorizontal: theme.space['8'],
+    },
+    button: {
+      paddingVertical: theme.space['3'],
+      paddingHorizontal: theme.space['4'],
+      backgroundColor: theme.colors.gray[100],
+      borderRadius: theme.radii?.sm ?? 6,
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: theme.fontSizes?.md ?? 16,
+    },
   });
-} 
+};

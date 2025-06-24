54647c127a238103bf0158c6cdb9fea912c5a494
shivamj-0303 <shivam.jangid@bettrhq.com>
2025-06-24 10:21:09 +0530

    fix(test): expire build

--- ios/fastlane/scripts/ios_testflight_cleanup.rb
+++ ios/fastlane/scripts/ios_testflight_cleanup.rb
@@ -31,7 +31,7 @@ def ios_testflight_cleanup!(pr_number:, app_identifier:, api_key_id:, issuer_id:
         unless build
           FastlaneCore::UI.user_error!("Build with ID #{build.id} not found.")
         end
-          build.update(build_id: build_id_to_expire, attributes: { expired: true })
+          build.update(build_id: build.id, attributes: { expired: true })
 
         FastlaneCore::UI.success("Successfully expired build ID: #{build.id}")
 

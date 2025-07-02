3dae743830f3897195261f7184e16b6c9d02a3bc
shivamj-0303 <shivam.jangid@bettrhq.com>
2025-07-02 11:06:11 +0530

    CI: added Year to build number

--- ios/fastlane/scripts/ios_testflight_deploy.rb
+++ ios/fastlane/scripts/ios_testflight_deploy.rb
@@ -28,7 +28,7 @@ def ios_testflight_deploy!(options = {})
   # Set the build number using current datetime + PR number to ensure uniqueness across PR builds.
   increment_build_number(
     xcodeproj: xcodeproj,
-    build_number: "#{Time.now.strftime('%m%d.%H%M')}.#{pr_number}"
+    build_number: "#{Time.now.strftime('%Y%m%d.%H%M')}.#{pr_number}"
   )
 
   app_store_connect_api_key(

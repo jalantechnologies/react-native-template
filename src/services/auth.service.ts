0ba7d567b21bd7db2ec1e31181c19079f50d1bbf
Sarthak Gaikwad <sarthak.gaikwad@bettrhq.com>
2025-07-04 18:58:36 +0530

    refactor(AuthService): simplify sendOTP and verifyOTP methods by removing error handling and payload construction

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,41 +1,24 @@
-import logger from '../logger/logger';
 import { AccessToken, APIResponse, PhoneNumber } from '../types';
 
 import { APIService } from './api-service';
 
 export class AuthService extends APIService {
   sendOTP = async (phoneNumber: PhoneNumber): Promise<APIResponse> => {
-    try {
-      const payload = {
-        phone_number: {
-          country_code: phoneNumber.countryCode,
-          phone_number: phoneNumber.phoneNumber,
-        },
-      };
-
-      const response = await this.post('/accounts', payload);
-      return typeof response === 'string' ? JSON.parse(response) : response;
-    } catch (error) {
-      logger.error(`SendOTP service error: ${error}`);
-      throw error;
-    }
+    return this.post('/accounts', {
+      phone_number: {
+        country_code: phoneNumber.countryCode,
+        phone_number: phoneNumber.phoneNumber,
+      },
+    });
   };
 
   verifyOTP = async (otp: string, phoneNumber: PhoneNumber): Promise<APIResponse> => {
-    try {
-      const payload = {
-        phone_number: {
-          country_code: phoneNumber.countryCode,
-          phone_number: phoneNumber.phoneNumber,
-        },
-        otp_code: otp,
-      };
-
-      const response = await this.post<AccessToken>('/access-tokens', payload);
-      return typeof response === 'string' ? JSON.parse(response) : response;
-    } catch (error) {
-      logger.error(`VerifyOTP service error: ${error}`);
-      throw error;
-    }
+    return this.post<AccessToken>('/access-tokens', {
+      otp_code: otp,
+      phone_number: {
+        country_code: phoneNumber.countryCode,
+        phone_number: phoneNumber.phoneNumber,
+      },
+    });
   };
 }

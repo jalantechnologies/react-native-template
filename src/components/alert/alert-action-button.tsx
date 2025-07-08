eab04b075625bb1d20735e1efb799b2acae720ee
Akshatbari02 <akshatbari.ab@gmail.com>
2025-07-08 18:28:14 +0530

    fix: code format

--- src/components/alert/alert-action-button.tsx
+++ src/components/alert/alert-action-button.tsx
@@ -5,7 +5,12 @@ import Button from '../button/button';
 
 type NonLinkButtonKind = Exclude<ButtonKind, ButtonKind.LINK>;
 
-export const AlertActionButton: React.FC<AlertActionButtonProps> = ({ label, type, onPress, color }) => {
+export const AlertActionButton: React.FC<AlertActionButtonProps> = ({
+  label,
+  type,
+  onPress,
+  color,
+}) => {
   return (
     <Button onClick={onPress} kind={type as NonLinkButtonKind} color={color}>
       {label}

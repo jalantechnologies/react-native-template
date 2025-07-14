b95ce4604b177220008afb76bd4c47745e090a6f
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2025-07-14 11:59:15 +0530

    feat(progressbar): add themed progress bar component

--- src/components/progressbar/progressBar.test.tsx
+++ src/components/progressbar/progressBar.test.tsx
@@ -7,13 +7,11 @@ import appTheme from '../../app-theme';
 import ProgressBar from './progressbar';
 
 jest.mock('react-native-safe-area-context', () => {
-  const ReactLib = require('react'); 
   return {
     SafeAreaProvider: ({ children }: { children: React.ReactNode }) => children,
     useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
   };
 });
-
 
 jest.useFakeTimers();
 

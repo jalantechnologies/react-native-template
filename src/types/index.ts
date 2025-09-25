c392e0210f0ceaff042d99315f09934f25cf8808
Indukuri Likhitha <indukuri.likhita@bettrhq.com>
2025-09-25 19:10:31 +0530

    Include all Slider files for PR with updated changes

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export * from './auth';
 export * from './common';
 export * from './task';
 export * from './avatar';
+export * from './slider';
 export * from './alert';
 export * from './input';
 export * from './spinner';

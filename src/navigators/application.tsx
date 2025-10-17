fca8531f82802dc2470191e25a1d68f46d666afc
Likhitha Indukuri <indukuri.likhita@bettrhq.com>
2025-10-17 13:06:27 +0530

    changed application.tsx

--- src/navigators/application.tsx
+++ src/navigators/application.tsx
@@ -1,43 +1,3 @@
-// import { DdRumReactNavigationTracking } from '@datadog/mobile-react-navigation';
-// import { useFlipper } from '@react-navigation/devtools';
-// import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { Box } from 'native-base';
-// import React from 'react';
-// import { StatusBar } from 'react-native';
-
-// import { ApplicationStackParamList } from '../../@types/navigation';
-// import { Startup } from '../screens';
-
-// import MainNavigator from './main';
-
-// const Stack = createStackNavigator<ApplicationStackParamList>();
-
-// // @refresh reset
-// const ApplicationNavigator = () => {
-//   const navigationRef = useNavigationContainerRef();
-
-//   useFlipper(navigationRef);
-
-//   const onNavigationReady = () => {
-//     DdRumReactNavigationTracking.startTrackingViews(navigationRef.current);
-//   };
-
-//   return (
-//     <Box safeAreaTop flex={1}>
-//       <NavigationContainer ref={navigationRef} onReady={onNavigationReady}>
-//         <StatusBar />
-//         <Stack.Navigator screenOptions={{ headerShown: false }}>
-//           <Stack.Screen name="Startup" component={Startup} />
-//           <Stack.Screen name="Main" component={MainNavigator} />
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     </Box>
-//   );
-// };
-
-// export default ApplicationNavigator;
-
 import { DdRumReactNavigationTracking } from '@datadog/mobile-react-navigation';
 import { useFlipper } from '@react-navigation/devtools';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
@@ -47,9 +7,9 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 import { ApplicationStackParamList } from '../../@types/navigation';
-// import { Startup } from '../screens';
-// import MainNavigator from './main';
-import TempDropdownScreen from '../screens/Temp'; // 👈 import your test screen
+import { Startup } from '../screens';
+
+import MainNavigator from './main';
 
 const Stack = createStackNavigator<ApplicationStackParamList>();
 
@@ -68,8 +28,8 @@ const ApplicationNavigator = () => {
       <NavigationContainer ref={navigationRef} onReady={onNavigationReady}>
         <StatusBar />
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* 👇 Only render temp test screen */}
-          <Stack.Screen name="TempDropdown" component={TempDropdownScreen} />
+          <Stack.Screen name="Startup" component={Startup} />
+          <Stack.Screen name="Main" component={MainNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     </Box>
